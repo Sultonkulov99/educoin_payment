78939dca99082b3414d86c1282192bc264878c8a
Sultonkulov99 <abdukhoshim99@gmail.com>
2025-09-18 10:01:01 +0500

    bug fixed

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PaymentsModule } from './payments/payments.module';
+import { PrismaModule } from './database/prisma.module';
 
 @Module({
-  imports: [PaymentsModule],
+  imports: [PaymentsModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
